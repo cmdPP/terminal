5c2a6c64cdec97cdd6430fa51e59d4d80f2b2334
Russell Hueske <hueske.russ690@gmail.com>
2016-03-26 17:26:39 -0500

    Updated CMD constructor.

--- src/index.js
+++ src/index.js
@@ -5,9 +5,8 @@
 import CMD from 'cmdpp-core';
 import inquirer from 'inquirer';
 import fs from 'fs';
-import path from 'path';
 import jsonfile from 'jsonfile';
-import { getPath, mkdir } from './utils';
+import { getPath } from './utils';
 
 var question = {
     type: 'input',
@@ -15,68 +14,65 @@ var question = {
     message: 'Data: 0 B | Money: $0 >'
 };
 
-var fsCheck = (fileName, cb) => {
-    // fs.access(fileName, fs.F_OK | fs.R_OK | fs.W_OK, (err) => {
-    //     if (err) {
-    //         console.error(err);
-    //         process.exit(1);
-    //     }
-    //     cb();
-    // });
-
+var fsCheck = (fileName) => {
     try {
         fs.accessSync(fileName);
     } catch (err) {
-        console.error(err);
         return false;
     }
     return true;
 };
 
+const saveDir = getPath('.cmdPP');
 const savePath = getPath('.cmdPP', 'save.json');
 
+if (!fsCheck(saveDir)) {
+    console.warn(`Save directory does not exist. Creating now at ${saveDir}`);
+    fs.mkdirSync(saveDir);
+}
+if (!fsCheck(savePath)) {
+    console.warn(`Save file does not exist. Creating now at ${savePath}`);
+    fs.closeSync(fs.openSync(savePath, 'w'));
+}
+
 var cmd = new CMD({
-    save: (cmdData) => {
-        mkdir('.cmdPP');
-        if (fsCheck(savePath)) {
+    debug: false,
+    funcs: {
+        save: (cmdData) => {
             jsonfile.writeFileSync(savePath, cmdData, { spaces: 2 });
-        } else {
-            console.error('Cannot access save.');
-        }
-    },
-    load: () => {
-        if (fsCheck(savePath)) {
+        },
+        load: () => {
             return jsonfile.readFileSync(savePath);
-        } else {
-            mkdir('.cmdPP');
-            // jsonfile.writeFileSync(savePath, {
-            //     data: 0,
-            //     money: 0,
-            //     increment: 1,
-            //     autoIncrement: 0,
-            //     unlocked: []
-            // }, { spaces: 2 });
-            // return jsonfile.readFileSync(savePath);
-            return null;
+        },
+        update: (cmdObj) => {
+            question.message = `Data: ${cmdObj.formatBytes()} | Money: $${cmdObj.money} >`;
+        },
+        reset: () => {
+            fs.unlinkSync(savePath);
         }
     },
-    update: (cmdObj) => {
-        // ui.updateBottomBar(`Data: ${cmdObj.formatBytes()}\tMoney: $${cmdObj.money}`);
-        question.message = `Data: ${cmdObj.formatBytes()} | Money: $${cmdObj.money} >`;
-    },
-    reset: (cmdObj) => {
-        fs.unlinkSync(savePath);
+    commandProvider: function() {
+        return {
+            quit: {
+                func: () => {
+                    this.command('save');
+                    this.respond('Now exiting.');
+                    process.exit(0);
+                },
+                desc: "Saves and exits game.",
+                unlocked: true
+            }
+        };
     }
 });
 
 function createInquiry() {
     var inquirerCB = (ans) => {
-        // console.log(ans);
-        if (ans.command === "quit" || ans.command === "q") {
-            cmd.command('save');
-            cmd.respond("Now exiting.");
-            process.exit(0);
-        }
+        // if (ans.command === "quit" || ans.command === "q") {
+        //     cmd.command('save');
+        //     cmd.respond("Now exiting.");
+        //     process.exit(0);
+        // }
         cmd.command(ans.command);
         createInquiry();
     };
